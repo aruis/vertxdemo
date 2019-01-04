8a20e547103766688feabc566a2c41ba26eba07c
Liu Rui <lovearuis@gmail.com>
2019-01-04 13:09:17 +0800

    初步实现 chat

--- src/main/groovy/com/aruistar/vertxdemo/web/HttpVerticle.groovy
+++ src/main/groovy/com/aruistar/vertxdemo/web/HttpVerticle.groovy
@@ -2,73 +2,89 @@ package com.aruistar.vertxdemo.web
 
 import groovy.util.logging.Slf4j
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.DeploymentOptions
-import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
-import io.vertx.ext.bridge.PermittedOptions
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.handler.CookieHandler
+import io.vertx.ext.web.handler.SessionHandler
 import io.vertx.ext.web.handler.StaticHandler
-import io.vertx.ext.web.handler.sockjs.BridgeOptions
 import io.vertx.ext.web.handler.sockjs.SockJSHandler
+import io.vertx.ext.web.sstore.LocalSessionStore
 
 @Slf4j
 class HttpVerticle extends AbstractVerticle {
 
-
-    static void main(String[] args) {
-        Vertx.vertx().deployVerticle(new HttpVerticle(), new DeploymentOptions([config: [
-                "http": [
-                        "port": 8899
-                ],
-                "db"  : [
-                        "host"    : "127.0.0.1",
-                        "database": "studypg",
-                        "user"    : "postgres",
-                        "password": "",
-                        "maxsize" : 6
-                ]
-        ]]))
-    }
-
     @Override
     void start() throws Exception {
 
-        boolean usingNative = vertx.isNativeTransportEnabled()
-
-        log.info("Running with native: " + usingNative)
-
         def httpConfig = config().getJsonObject("http", new JsonObject([port: 8899]))
-        def dbConfig = config().getJsonObject("db")
 
-        if (!dbConfig) {
-            dbConfig = [
-                    "host"    : "127.0.0.1",
-                    "database": "studypg",
-                    "user"    : "postgres",
-                    "password": "",
-                    "maxsize" : 6
-            ]
-        }
-
-        vertx.deployVerticle(new DatabaseVerticle(), new DeploymentOptions([config: dbConfig]))
-
-
-        Router router = Router.router(vertx);
+        Router router = Router.router(vertx)
         def port = httpConfig.getInteger("port", 8899)
 
-        // Allow events for the designated addresses in/out of the event bus bridge
-        BridgeOptions opts = new BridgeOptions()
-                .addOutboundPermitted(new PermittedOptions().setAddress("flunk"));
+        def bodyHandler = BodyHandler.create()
+
+        def opts = [
+                inboundPermitteds : [
+                        [
+                                address: "chat.to.server"
+                        ]
+                ],
+                outboundPermitteds: [
+                        [
+                                address: "chat.to.client"
+                        ]
+                ]
+        ]
 
         // Create the event bus bridge and add it to the router.
         SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
         router.route("/messagebus/*").handler(ebHandler)
 
+        def store = LocalSessionStore.create(vertx)
+        def sessionHandler = SessionHandler.create(store)
+        def cookieHandler = CookieHandler.create()
+        router.route("/nickname")
+                .handler(cookieHandler)
+                .handler(sessionHandler)
+
+        router.post("/nickname")
+                .handler(bodyHandler)
+                .handler({ routingContext ->
+
+            def response = routingContext.response()
+            def nickname = routingContext.getBodyAsJson().nickname
+
+            routingContext.session().put("nickname", nickname)
+
+            response.end(nickname)
+        })
+
+        router.get("/nickname")
+                .handler({ routingContext ->
+
+            def response = routingContext.response()
+            def nickname = routingContext.session().get("nickname")
+
+            if (nickname)
+                response.end(nickname)
+            else
+                response.end()
+        })
+
         // Create a router endpoint for the static content.
         router.route().handler(StaticHandler.create());
 
+        def eb = vertx.eventBus()
+
+        eb.consumer("chat.to.server").handler({ message ->
+            def body = message.body()
+            body.date = new Date().format("yyyy-MM-dd HH:mm:ss:SSS")
+            eb.publish("chat.to.client", body)
+        })
+
         // Start the web server and tell it to use the router to handle requests.
-        vertx.createHttpServer().requestHandler(router.&accept)
+        vertx.createHttpServer().requestHandler(router)
                 .listen(port, { ar ->
             if (ar.succeeded()) {
                 log.info("server is running on port " + port)
